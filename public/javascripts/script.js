766d593fbd93a7b8868c5bbf50ad14778c476169
Kenneth Owusu <keninchrist4eva@yahoo.com>
2018-05-05 18:05:49 +0000

    theme changed

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,75 +1,20 @@
-$('#menu-icon').on('click',function(){
-  $("html").toggleClass('show-menu');
-})
-
-$('.main-nav').on('click',function(e){
-  var target = $(e.target);
-  var targetId = target.attr("id");
-  $('.sub-navs#'+targetId).show();
-})
-
-$('.sub-nav').on('click',function(e){
-  var target = $(e.target);
-  var targetId = target.attr("id");
-  $('.sub-sub-navs#'+targetId).show();
-})
-
-
-$('.back-cont,.back').on('click',function(e){
-  $(e.target).parents('.sub-navs').first().hide();
-})
-
-$('.sub-back-cont,.sub-back').on('click',function(e){
-  $(e.target).parents('.sub-sub-navs').first().hide();
-})
-
-$('.main-nav').hover(function(e){
-  var target = $(e.target);
-  var targetId = target.attr("id");
-  var navcont  = $('.nav-container#'+targetId);
-  if(navcont.css("display")=="none"){
-    navcont.addClass('force-flex');
+$('#nav-overlay').on('click',function(e){
+  var clicked = $(e.target);
+  if(clicked.is('.nav-container') || clicked.parents().is('.nav-container')){
+    return;
   }else{
-    navcont.removeClass('force-flex');
+
+      $('.nav-container').removeClass('no-translate');
+      $("#nav-overlay").hide(300,'linear');
   }
-},function(e){
-  var target = $(e.target);
-  var targetId = target.attr("id");
-  var navcont  = $('.nav-container#'+targetId);
-  if(navcont.css("display")=="none"){
-    navcont.addClass('force-flex');
-  }else{
-    navcont.removeClass('force-flex');
-  }
-});
+})
 
-// $('.nav-container').on("mouseenter",function(e){
-//   $(e.target).toggleClass("d-flex");
-// });
-//
-//
+$('#menu-img,.menu').on('click',function(){
+  $('#nav-overlay').show(300,'linear');
+  $('.nav-container').addClass('no-translate');
+})
 
-
-
-
-
-
-//=========================
-//FOR SLICK
-//=========================
-$(".banner-container").slick({
-  infinite: true,
-   arrows: true,
-   dots:true,
-   autoplay:true,
-   autoplayspeed:400
-});
-
-
-$(".products-section").slick({
-   arrows: true,
-   dots:true,
-   sildesToScroll:2
-
-
-});
+$('#close,.close').on('click',function(){
+    $("#nav-overlay").hide(300,'linear');
+    $('.nav-container').addClass('no-translate');
+})
